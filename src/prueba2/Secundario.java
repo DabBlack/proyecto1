3ad382550af2f797d8b9b6cdef111bd7c2315a51
DabBlack <davidalgar2@gmail.com>
2022-04-06 13:09:31 +0200

    Cambios y rama "Nueva"

--- src/prueba2/Secundario.java
+++ src/prueba2/Secundario.java
@@ -3,7 +3,7 @@ package prueba2;
 public class Secundario {
 
 	public static void main(String[] args) {
-		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba 2");
 	}
 
 }
