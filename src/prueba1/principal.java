d9a26bad37dffcab031d99ff1b33cfbc694ddcb1
DabBlack <davidalgar2@gmail.com>
2022-04-06 11:21:02 +0200

    Cambio clase principal

--- src/prueba1/principal.java
+++ src/prueba1/principal.java
@@ -3,6 +3,7 @@ package prueba1;
 public class principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo, modificacion 2");
+		System.out.println("Hola Mundo");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 	}
 }
